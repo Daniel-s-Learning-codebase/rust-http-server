9a359720e72f265b543b76fac6fc43c73c228729
danielthapa <dtdaniel27@gmail.com>
2023-07-16 21:51:35 +0545

    organizing code to  modules and submodules

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,11 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
-    let get = Method::GET("hello".to_string());
-    let delete = Method::DELETE(293);
-    let post = Method::POST;
-    let put = Method::PUT;
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET(String),
-    DELETE(u64),
-    POST,
-    PUT = 5, // value can be assigned manually
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATH,
 }
