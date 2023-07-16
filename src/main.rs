65a6a1041ca51d94ac7390c0556cba78b5d729cc
danielthapa <dtdaniel27@gmail.com>
2023-07-16 21:32:37 +0545

    add enums

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    // let url = String::from("127.0.0.1:8080");
+    let get = Method::GET("hello".to_string());
+    let delete = Method::DELETE(293);
+    let post = Method::POST;
+    let put = Method::PUT;
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -16,4 +19,22 @@ impl Server {
     fn run(self) {
         println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query: String,
+    method: Method,
+}
+
+enum Method {
+    GET(String),
+    DELETE(u64),
+    POST,
+    PUT = 5, // value can be assigned manually
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATH,
 }
