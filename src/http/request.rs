4ab0a207bd74832c6a98e7acca359e7e6b04d5f1
danielthapa <dtdaniel27@gmail.com>
2023-07-21 00:09:04 +0545

    request mod update

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,31 +1,54 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str;
 use std::str::Utf8Error;
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
-}
-
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        match str::from_utf8(buf) {
-            Ok(request) => {}
-            Err(_) => return Err(ParseError::InvalidEncoding),
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        };
+
+        let method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find("?") {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i]
         }
 
-        let request = str::from_utf8(buf)?;
+        Ok::<Request<'buf>, Self::Error>(Self {
+            path,
+            query_string,
+            method,
+        });
+
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+    None
 }
 
 impl Display for ParseError {
@@ -40,9 +63,15 @@ impl Debug for ParseError {
     }
 }
 
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
+}
+
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
-        Self::InvalidEncoding;
+        Self::InvalidEncoding
     }
 }
 
